87a7dcac190ed907921c988a03674396de5bbba7
MIRZOULUGBEK9008 <ulugbekkhudoyberdiyev9008@gmail.com>
2022-04-27 23:25:32 +0500

    Add switch toggle and prefix, minify.

--- js/main.js
+++ js/main.js
@@ -1,36 +1,20 @@
 // VARIABLES
-const elAnnually = document.getElementById("annually");
-const elMonthly = document.getElementById("monthly");
-const elBasic = document.getElementById("basic");
-const elPro = document.getElementById("pro");
-const elMaster = document.getElementById("master");
-const elSlider = document.querySelector(".slider-label-monthly");
+const elsPricingMode = document.querySelectorAll('input[type=radio]');
+const elsPricing = document.querySelectorAll('.pricing');
 
 // FUNCTION
-elAnnually.addEventListener("click", function () {
-  elBasic.textContent = ("19.99");
-  elPro.textContent = ("24.99");
-  elMaster.textContent = ("39.99");
-  elSlider.classList.remove("slider-label-monthly");
-});
+elsPricingMode.forEach(function (elPricing) {
+  elPricing.addEventListener('change', function () {
 
-elMonthly.addEventListener("click", function () {
-  elBasic.textContent = ("199.99");
-  elPro.textContent = ("249.99");
-  elMaster.textContent = ("399.99");
-  elSlider.classList.add("slider-label-monthly");
-});
-
-elSlider.addEventListener("click", function() {
-  let a = elSlider.classList.toggle("slider-label-monthly");
-  if(a == true){
-    elBasic.textContent = ("199.99");
-    elPro.textContent = ("249.99");
-    elMaster.textContent = ("399.99");
-  }else{
-    elBasic.textContent = ("19.99");
-    elPro.textContent = ("24.99");
-    elMaster.textContent = ("39.99");
-    elSlider.classList.remove("slider-label-monthly");
-  }
+    const period = elPricing.value;
+    if (period === 'annually') {
+      elsPricing.forEach(function (elPrice) {
+        elPrice.textContent = elPrice.closest('.pricing-list-item').dataset.paymentAnnually;
+      });
+    } else {
+      elsPricing.forEach(function (elPrice) {
+        elPrice.textContent = elPrice.closest('.pricing-list-item').dataset.paymentMonthly;
+      });
+    }
+  });
 });
